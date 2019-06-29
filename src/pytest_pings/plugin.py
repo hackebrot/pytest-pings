61a82f3b332c11b2b5e12504a15a4445e957e193
Raphael Pierzina <raphael@hackebrot.de>
2019-06-29 22:13:15 +0200

    Develop telemetry_server and pings fixtures

--- src/pytest_pings/plugin.py
+++ src/pytest_pings/plugin.py
@@ -1,11 +1,26 @@
+import typing
 import pytest
+from aiohttp import web
 
 
-class Server:
-    """Telemetry ping server."""
+@pytest.fixture(name="pings")
+def fixture_pings() -> typing.List:
+    """Returns a list that telemetry_server stores Telemetry pings to."""
+    return []
 
 
-@pytest.fixture
-def server():
-    return Server()
+@pytest.fixture(name="telemetry_server")
+async def fixture_telemetry_server(aiohttp_server, pings: typing.List):
+    """Return a TestServer that stores Telemetry pings to the pings fixture.
 
+    Submit pings via POST /submit/telemetry/{id}/{type}/
+    """
+
+    async def submit(request):
+        """Handler that stores Telemetry pings."""
+        pings.append(await request.json())
+        return web.Response(status=200)
+
+    app = web.Application()
+    app.add_routes([web.post("/submit/telemetry/{id}/{type}/{info:.*}", submit)])
+    return await aiohttp_server(app)
